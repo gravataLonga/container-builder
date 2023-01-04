7d2c43bbd3e3059e8d674dbd7b5612572d1b4ba1
Jonathan Fontes <jonathan.alexey@icloud.com>
2023-01-04 21:58:11 +0000

    --wip-- [skip ci]

--- src/ContainerBuilder.php
+++ src/ContainerBuilder.php
@@ -7,13 +7,36 @@ use Psr\Container\ContainerInterface;
 
 final class ContainerBuilder
 {
+    private Container $container;
+
+    /** @var \Gravatalonga\ContainerBuilder\ServiceProvider[] $providers */
+    private array $providers = [];
 
     public function __construct()
     {
+        $this->container = new Container();
+    }
+
+    public function services(array $providers): void
+    {
+        $this->providers = $providers;
     }
 
     public function build(): ContainerInterface
     {
-        return new Container();
+        foreach ($this->providers as $provider) {
+            $factories = $provider->factories();
+            foreach ($factories as $entry => $factory) {
+                $this->container->share($entry, $factory);
+            }
+        }
+
+        foreach ($this->providers as $provider) {
+            $extensions = $provider->extensions();
+            foreach ($extensions as $entry => $extension) {
+                $this->container->extend($entry, $extension);
+            }
+        }
+        return $this->container;
     }
 }
--- tests/ContainerBuilderTest.php
+++ tests/ContainerBuilderTest.php
@@ -4,6 +4,7 @@ namespace Tests;
 
 use Gravatalonga\Container\Container;
 use Gravatalonga\ContainerBuilder\ContainerBuilder;
+use Gravatalonga\ContainerBuilder\ServiceProvider;
 use PHPUnit\Framework\TestCase;
 use Psr\Container\ContainerInterface;
 
@@ -19,5 +20,61 @@ class ContainerBuilderTest extends TestCase
         $container = $builder->build();
 
         $this->assertInstanceOf(ContainerInterface::class, $container);
+    }
+
+    /**
+     * @test
+     */
+    public function can_register_service_provider ()
+    {
+        $builder = new ContainerBuilder();
+        $builder->services([
+            new class() implements ServiceProvider
+            {
+                public function factories(): array
+                {
+                    return ['env' => function() { return 'PRD'; } ];
+                }
+
+                public function extensions(): array
+                {
+                    return [];
+                }
+            }
+        ]);
+
+        $container = $builder->build();
+
+        $this->assertTrue($container->has('env'));
+        $this->assertEquals('PRD', $container->get('env'));
+    }
+
+    /**
+     * @test
+     */
+    public function can_extend_entries ()
+    {
+        $builder = new ContainerBuilder();
+        $builder->services([
+            new class() implements ServiceProvider
+            {
+                public function factories(): array
+                {
+                    return ['env' => function() { return 'PRD'; } ];
+                }
+
+                public function extensions(): array
+                {
+                    return [
+                        'env' => function() { return 'DEV'; }
+                    ];
+                }
+            }
+        ]);
+
+        $container = $builder->build();
+
+        $this->assertTrue($container->has('env'));
+        $this->assertEquals('DEV', $container->get('env'));
     }
 }
